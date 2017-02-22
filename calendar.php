510466acb9754c69977183b4196d36eea1c61026
Shadow1356 <nhurst@ashland.edu>
2017-02-21 19:53:13 -0500

    Title Change
    Changed the title to ACM-Calendar

--- calendar.php
+++ calendar.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>ACM</title>
+        <title>ACM-Calendar</title>
 
         <!--CSS files-->
         <link rel="stylesheet" type="text/css" href="style/main.css">
