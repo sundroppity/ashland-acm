c86e4b7e4d03924baa99cea0f8ce9077d061cd35
Nathan Ahrens <redninja2@users.noreply.github.com>
2017-02-21 19:24:02 -0500

    Add files via upload
    Uploaded initial files

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>ACM</title>
+
+        <!--CSS files-->
+        <link rel="stylesheet" type="text/css" href="style/main.css">
+    </head>
+    <body>
+        <!--Login button-->
+        <header>
+        </header>
+        
+        <!--left link list-->
+        <nav>
+            <!--List of links-->
+            <ul>
+                <li><a href="somepage.html">Some link</a></li>
+                <li><a href="somepage.html">Another Link</a></li>
+            </ul>
+        </nav>        
+
+        <section>
+            <article class="announcement">
+            </article>
+            <article class="updates">
+            </article>
+        </section>        
+
+        <!--right side if we need it-->
+        <aside>
+        </aside>
+
+        <footer>
+        </footer>
+    </body>
+</html>
+
---
+++ projects/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Projects</title>
+
+        <!--CSS files-->
+        <link rel="stylesheet" type="text/css" href="style/main.css">
+    </head>
+    <body>
+        <!--Login button-->
+        <header>
+        </header>
+        
+        <!--left link list-->
+        <nav>
+            <!--List of links-->
+            <ul>
+                
+            </ul>
+        </nav>        
+
+        <section>
+            <article class="announcement">
+                <p>These are projects</p>
+            </article>
+            <article class="updates">
+            </article>
+        </section>        
+
+        <!--right side if we need it-->
+        <aside>
+        </aside>
+
+        <footer>
+        </footer>
+    </body>
+</html>
