ac4594a9e541a6eca89d422ee2c25c6e56440f9f
Shadow1356 <nhurst@ashland.edu>
2017-03-14 07:49:17 -0400

    Calendar integration
    Works in some browsers

--- calendar.php
+++ calendar.php
@@ -23,6 +23,8 @@
 
         <section>
             <article class="calendar">
+			<iframe src="https://calendar.google.com/calendar/embed?src=ashland.edu_kjv82nbu8591cqcb7cei7871eo%40group.calendar.google.com&ctz=America/New_York"
+			style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
             </article>
         </section>        
 
